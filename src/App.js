d2cad27bfa72721f227748216d93a3d62c705472
MAYUBXG <medaymenhmila@gmail.com>
2022-08-18 16:42:18 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+
+import './App.css';
+
+
+import Menu from './components/Menu';
+import Sign from './components/Sign';
+
+
+
+function App() {
+  return (
+    <div>
+    <Menu/>
+    <Sign/>
+  
+    </div>
+    
+    
+  );
+}
+
+export default App;
